55c06de49afb1ad659790f50e90c7bad76c5f752
Alvaro Garcia <derecerca@gmail.com>
2017-08-21 22:15:32 +0200

    Refactor: extract explaining method to increase quality

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -11,21 +11,15 @@ class GildedRose(var items: Array<Item>) {
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                }
+                increaseQuality(i)
 
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
+                        increaseQuality(i)
                     }
 
                     if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
+                        increaseQuality(i)
                     }
                 }
             }
@@ -46,11 +40,15 @@ class GildedRose(var items: Array<Item>) {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    increaseQuality(i)
                 }
             }
+        }
+    }
+
+    private fun increaseQuality(i: Int) {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
         }
     }
 
