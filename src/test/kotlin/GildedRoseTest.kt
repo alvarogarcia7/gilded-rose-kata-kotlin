8a5887a20abf84616c6068c2ab6638823df6117b
Alvaro Garcia <derecerca@gmail.com>
2017-08-21 20:19:07 +0200

    Refactor: apply last refactor to other test cases

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -8,19 +8,18 @@ class GildedRoseTest {
     @Test
     fun shouldLowerSellInAndQualityOfItemAtTheEndOfEachDay() {
         val result = updateQualityTo(arrayOf(Item("anItem", 3, 5)))
+
         assertEquals("anItem", result[0].name)
         assertEquals(2, result[0].sellIn)
         assertEquals(4, result[0].quality)
     }
 
     @Test fun shouldLowerSellInAndQualityOfAllItemsAtTheEndOfEachDay() {
-        val items = arrayOf(
+        val items = updateQualityTo(arrayOf(
                 Item("anItem", 3, 5),
-                Item("anItem", 3, 5))
-        val app = GildedRose(items)
-        app.updateQuality()
+                Item("anItem", 3, 5)))
 
-        for (item in app.items) {
+        for (item in items) {
             assertEquals("anItem", item.name)
             assertEquals(2, item.sellIn)
             assertEquals(4, item.quality)
@@ -29,12 +28,10 @@ class GildedRoseTest {
 
     @Test
     fun shouldQualityBeNeverNegative() {
-        val items = arrayOf(Item("anItem", 10, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("anItem", app.items[0].name)
-        assertEquals(9, app.items[0].sellIn)
-        assertEquals(0, app.items[0].quality)
+        val items = updateQualityTo(arrayOf(Item("anItem", 10, 0)))
+        assertEquals("anItem", items[0].name)
+        assertEquals(9, items[0].sellIn)
+        assertEquals(0, items[0].quality)
     }
 
     private fun updateQualityTo(items: Array<Item>): Array<Item> {
