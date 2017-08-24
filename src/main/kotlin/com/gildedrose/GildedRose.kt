1cc240bca0349c935a77dbbb850e8c3b55fe3e35
Alvaro Garcia <derecerca@gmail.com>
2017-08-24 07:16:49 +0200

    Refactor: split condition (manual)

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,7 +7,10 @@ class GildedRose(var items: Array<Item>) {
             if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 continue
             }
-            if ((items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert"))) {
+            if ((items[i].name.equals("Aged Brie"))){
+                increaseQuality(items[i])
+            } else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC " +
+                    "concert")) {
                 increaseQuality(items[i])
 
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
