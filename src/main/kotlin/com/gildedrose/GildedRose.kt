e371eaddb02d64b61ad768bec0a4db9575612326
Alvaro Garcia <derecerca@gmail.com>
2017-08-25 08:13:11 +0200

    Refactor: remove dead code

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -33,16 +33,6 @@ class GildedRose(var items: Array<Item>) {
             }
 
             items[i].sellIn = items[i].sellIn - 1
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name.equals("Aged Brie")) {
-                    increaseQuality(items[i])
-                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    items[i].quality = 0
-                } else {
-                    decreaseQuality(i)
-                }
-            }
         }
     }
 
