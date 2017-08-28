c7f2c41f5a9c1b9a5f965396a40e37046c3e1579
Alvaro Garcia <derecerca@gmail.com>
2017-08-29 00:39:27 +0200

    Refactor: push behaviour from GildedRose to new object MyItem

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,48 +1,11 @@
 package com.gildedrose
 
+
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                continue
-            }
-            if (items[i].name.equals("Aged Brie")) {
-                increaseQuality(items[i])
-                if (items[i].sellIn < 0) {
-                    increaseQuality(items[i])
-                }
-            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                increaseQuality(items[i])
-
-                if (items[i].sellIn < 11) {
-                    increaseQuality(items[i])
-                }
-
-                if (items[i].sellIn < 6) {
-                    increaseQuality(items[i])
-                }
-                if (items[i].sellIn < 0) {
-                    items[i].quality = 0
-                }
-            } else {
-                decreaseQuality(i)
-                if (items[i].sellIn < 0) {
-                    decreaseQuality(i)
-                }
-            }
-
-            items[i].sellIn = items[i].sellIn - 1
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name.equals("Aged Brie")) {
-                    increaseQuality(items[i])
-                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    items[i].quality = 0
-                } else {
-                    decreaseQuality(i)
-                }
-            }
+            items[i] = ItemFactory.aNew(items[i]).updateQuality().toDTO()
         }
     }
 
--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -1,3 +1,92 @@
 package com.gildedrose
 
 data class Item(var name: String, var sellIn: Int, var quality: Int)
+
+class ImmutableItem(item: Item) : MyItem(item)
+
+class AgedBrie(item: Item) : MyItem(item)
+
+open class MyItem {
+
+    private var item: Item
+
+    constructor(item: Item){
+        this.item = item
+    }
+
+    fun updateQuality(): MyItem {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return this
+        }
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality()
+            if (item.sellIn < 0) {
+                increaseQuality()
+            }
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQuality()
+
+            if (item.sellIn < 11) {
+                increaseQuality()
+            }
+
+            if (item.sellIn < 6) {
+                increaseQuality()
+            }
+            if (item.sellIn < 0) {
+                item.quality = 0
+            }
+        } else {
+            decreaseQuality()
+            if (item.sellIn < 0) {
+                decreaseQuality()
+            }
+        }
+
+        item.sellIn = item.sellIn - 1
+
+        if (item.sellIn < 0) {
+            if (item.name.equals("Aged Brie")) {
+                increaseQuality()
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = 0
+            } else {
+                decreaseQuality()
+            }
+        }
+
+        return this
+    }
+
+    fun toDTO(): Item {
+        return Item(item.name, item.sellIn, item.quality)
+    }
+
+    private fun decreaseQuality() {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1
+        }
+    }
+
+    private fun increaseQuality() {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+    }
+}
+
+class BackstagePasses(name: Item) : MyItem(name)
+
+object ItemFactory {
+    fun aNew(name: Item): MyItem {
+        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+            return ImmutableItem(name)
+        } else if (name.equals("Aged Brie")) {
+            return AgedBrie(name)
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return BackstagePasses(name)
+        } else {
+            return MyItem(name)
+        }
+    }
+}
