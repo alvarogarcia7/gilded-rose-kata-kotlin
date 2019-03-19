b11d45099627006140236de138b70c74915108f5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-03-19 12:32:28 +0100

    Refactor: change assert style + prefer using kotlin rich method names

--- src/test/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/GildedRoseTest.kt
@@ -6,81 +6,76 @@ import org.junit.Test
 class GildedRoseTest {
 
     @Test
-    fun shouldLowerSellInAndQualityOfItemAtTheEndOfEachDay() {
-        val result = updateQualityTo(arrayOf(Item("anItem", 3, 5)))
+    fun `should Lower SellIn and Quality of item at the end of each day`() {
+        val result = updateQualityTo(arrayOf(anItem(3, 5)))
 
-        assertEquals("anItem", result[0].name)
-        assertEquals(2, result[0].sellIn)
-        assertEquals(4, result[0].quality)
+        assertEquals(anItem(2, 4), result[0])
     }
 
-    @Test fun shouldLowerSellInAndQualityOfAllItemsAtTheEndOfEachDay() {
+    @Test
+    fun `should lower SellIn and Quality of all items at the end of each day`() {
         val items = updateQualityTo(arrayOf(
-                Item("anItem", 3, 5),
-                Item("anItem", 3, 5)))
+                anItem(3, 5),
+                anItem(3, 5)))
 
         for (item in items) {
-            assertEquals("anItem", item.name)
-            assertEquals(2, item.sellIn)
-            assertEquals(4, item.quality)
+            assertEquals(anItem(2, 4), item)
         }
     }
 
     @Test
-    fun shouldQualityBeNeverNegative() {
-        val items = updateQualityTo(arrayOf(Item("anItem", 10, 0)))
-        assertEquals("anItem", items[0].name)
-        assertEquals(9, items[0].sellIn)
-        assertEquals(0, items[0].quality)
+    fun `should Quality be never negative`() {
+        val items = updateQualityTo(arrayOf(anItem(10, 0)))
+
+        assertEquals(anItem(9, 0), items[0])
     }
 
     @Test
-    fun shouldQualityDecreaseTwiceIfTheItemSellPeriodIsEnded() {
-        val items = updateQualityTo(arrayOf(Item("anItem", 0, 2)))
-        assertEquals("anItem", items[0].name)
-        assertEquals(-1, items[0].sellIn)
-        assertEquals(0, items[0].quality)
+    fun `should Quality decrease twice if the item is expired`() {
+        val items = updateQualityTo(arrayOf(anItem(0, 2)))
+
+        assertEquals(anItem(-1, 0), items[0])
+    }
+
+    @Test
+    fun `aged brie actually increases when a day passes by`() {
+        val items = updateQualityTo(arrayOf(agedBrie(1, 2)))
+
+        assertEquals(agedBrie(0, 3), items[0])
     }
 
 
     @Test
-    fun agedBrieActuallyIncreasesWhenADayPassesBy() {
-        val items = updateQualityTo(arrayOf(Item("Aged Brie", 1, 2)))
-        assertEquals(0, items[0].sellIn)
-        assertEquals(3, items[0].quality)
+    fun `aged brie actually increases double fast when a day passes by and the product is expired`() {
+        val items = updateQualityTo(arrayOf(agedBrie(0, 2)))
+
+        assertEquals(agedBrie(-1, 4), items[0])
     }
 
     @Test
-    fun agedBrieActuallyIncreasesDoubleFastWhenADayPassesByAndTheSellInHasExpired() {
-        val items = updateQualityTo(arrayOf(Item("Aged Brie", 0, 2)))
-        assertEquals(-1, items[0].sellIn)
-        assertEquals(4, items[0].quality)
+    fun `Quality should never be larger than 50`() {
+        val items = updateQualityTo(arrayOf(agedBrie(0, 50)))
+
+        assertEquals(agedBrie(-1, 50), items[0])
     }
 
     @Test
-    fun qualityShouldNotBeLargerThan50() {
-        val items = updateQualityTo(arrayOf(Item("Aged Brie", 0, 50)))
-        assertEquals(-1, items[0].sellIn)
-        assertEquals(50, items[0].quality)
-    }
-
-    @Test
-    fun sulfurasNeverShouldBeSoldOrDecreasesInQuality_caseExpired() {
+    fun `sulfuras never should be sold or decrased in Quality case Expired`() {
         `"Sulfuras", being a legendary item, never has to be sold or decreases in Quality`(0, 5)
     }
 
     @Test
-    fun sulfurasNeverShouldBeSoldOrDecreasesInQuality_caseNotExpired() {
+    fun `sulfuras never should be sold or decrased in Quality case Not Expired`() {
         `"Sulfuras", being a legendary item, never has to be sold or decreases in Quality`(1, 2)
     }
 
     @Test
-    fun sulfurasNeverShouldBeSoldOrDecreasesInQuality_caseMaximumNormalQuality() {
+    fun `sulfuras never should be sold or decrased in Quality case Maximum Normal Quality`() {
         `"Sulfuras", being a legendary item, never has to be sold or decreases in Quality`(1, 50)
     }
 
     @Test
-    fun sulfurasNeverShouldBeSoldOrDecreasesInQuality_caseMaximumQuality() {
+    fun `sulfuras never should be sold or decrased in Quality case Maximum Quality`() {
         `"Sulfuras", being a legendary item, never has to be sold or decreases in Quality`(1, 80)
     }
 
@@ -88,38 +83,44 @@ class GildedRoseTest {
     fun `backstage passes increases quality when SellIn value approaches -  Case Tier 3`() {
         val x = 30
         val items = updateQualityTo(arrayOf(backstagePass(11, x)))
-        assertEquals(10, items[0].sellIn)
-        assertEquals(x + 1, items[0].quality)
+
+        assertEquals(backstagePass(10, x + 1), items[0])
     }
 
     @Test
     fun `backstage passes increases quality when SellIn value approaches -  Case Tier 2`() {
         val x = 30
         val items = updateQualityTo(arrayOf(backstagePass(10, x)))
-        assertEquals(9, items[0].sellIn)
-        assertEquals(x + 2, items[0].quality)
+        
+        assertEquals(backstagePass(9, x + 2), items[0])
     }
 
     @Test
     fun `backstage passes increases quality when SellIn value approaches -  Case Tier 1`() {
         val x = 30
-        val items = updateQualityTo(arrayOf(backstagePass(5,x)))
-        assertEquals(4, items[0].sellIn)
-        assertEquals(x + 3, items[0].quality)
+        val items = updateQualityTo(arrayOf(backstagePass(5, x)))
+        
+        assertEquals(backstagePass(4, x + 3), items[0])
     }
 
     @Test
     fun `backstage passes increases quality when SellIn value approaches -  Expired`() {
-        val items = updateQualityTo(arrayOf(backstagePass(0,30)))
-        assertEquals(-1, items[0].sellIn)
-        assertEquals(0, items[0].quality)
+        val items = updateQualityTo(arrayOf(backstagePass(0, 30)))
+        
+        assertEquals(backstagePass(-1, 0), items[0])
     }
 
     private fun `"Sulfuras", being a legendary item, never has to be sold or decreases in Quality`(sellIn: Int, quality: Int) {
-        val items = updateQualityTo(arrayOf(Item("Sulfuras, Hand of Ragnaros", sellIn, quality)))
-        assertEquals(sellIn, items[0].sellIn)
-        assertEquals(quality, items[0].quality)
+        val items = updateQualityTo(arrayOf(sulfuras(sellIn, quality)))
+
+        assertEquals(sulfuras(sellIn, quality), items[0])
     }
+
+    private fun anItem(sellIn: Int, quality: Int) = Item("anItem", sellIn, quality)
+
+    private fun agedBrie(sellIn: Int, quality: Int) = Item("Aged Brie", sellIn, quality)
+
+    private fun sulfuras(sellIn: Int, quality: Int) = Item("Sulfuras, Hand of Ragnaros", sellIn, quality)
 
     private fun backstagePass(sellIn: Int, quality: Int) = Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality)
 
